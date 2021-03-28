443fe77400aeed08e2c0c3e472c681b010a2ed9c
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-03-28 19:41:01 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+	
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n, k;
+		cin >> n >> k;
+		// to calculate 'd', find the ceil of (n / k)
+		int d = (n % k == 0 ? n / k : (n + k - 1) / k);
+		// to calculate 'x', there will be two cases:
+		// 1. 'n' is not divisible 'k' = store 'n' mod 'k'
+		// 2. 'n' is divisible by 'k' = store 'k'
+		int x = (n % k != 0 ? n % k : k);
+		cout << d << " " << x << '\n';
+	}
+	return 0;
+}
